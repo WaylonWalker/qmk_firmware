a52d9a231a2b76024858f1ea98e255eac6c64904
Waylon Walker <waylon@waylonwalker.com>
2021-06-30 19:50:39 -0500

    wip

--- keyboards/crkbd/keymaps/waylonwalker/keymap.c
+++ keyboards/crkbd/keymaps/waylonwalker/keymap.c
@@ -20,35 +20,39 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include QMK_KEYBOARD_H
 #include <stdio.h>
 
+// Tap Dance definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_split_3x6_3(
-  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ESC,
-  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI,   MO(1),  KC_SPC,     KC_ENT,   MO(2), KC_RALT
-                                      //`--------------------------'  `--------------------------'
+  //,-----------------------------------------------------------------------.                    ,-----------------------------------------------------------------.
+       LT(1, KC_TAB),             KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
+  //|--------------------------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------------------|
+      LT(2, KC_LCTL),             KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+  //|--------------------------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------------------|
+      MT(MOD_RSFT, KC_ESC),     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  MT(MOD_HYPR, KC_ESC),
+  //|--------------------------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------------------|
+                                                   KC_LGUI,   KC_LCTL,  KC_LCTL,         LT(1, KC_ENT),   LT(2, KC_SPC), KC_RSFT
+                                               //`-----------------------------------'  `---------------------------------------'
 
   ),
 
   [1] = LAYOUT_split_3x6_3(
-  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, _______, _______, _______, _______, _______,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, _______, _______,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______,
-  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI, _______,  KC_SPC,     KC_ENT,   MO(3), KC_RALT
-                                      //`--------------------------'  `--------------------------'
+  //,--------------------------------------------------------------------.                    ,-----------------------------------------------------.
+      _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_DEL,
+  //|--------+--------+--------+--------+--------+-----------------------|                    |--------+--------+--------+--------+--------+--------|
+        MO(3), KC_END, KC_HOME, KC_PGDN , _______,    KC_MEDIA_PLAY_PAUSE,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT,  KC_F11,  KC_F12,
+  //|--------+--------+--------+--------+--------+-----------------------|                    |--------+--------+--------+--------+--------+--------|
+      KC_LSFT, _______, _______, _______, _______,    _______,                                  KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD,
+  //|--------+--------+--------+--------+--------+--------+-----------------------|  |--------+--------+--------+--------+--------+--------+--------|
+                                                         KC_LSFT, _______,  _______,     _______,   MO(3), _______
+                                                     //`--------------------------'  `--------------------------'
   ),
 
   [2] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+       KC_TAB, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LCTL, _______, _______, _______, _______, _______,                      KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,  KC_GRV,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -71,3 +75,48 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   )
 };
 
+#ifdef RGB_MATRIX_ENABLE
+#   define RGB_MATRIX_KEYPRESSES // reacts to keypresses
+// #   define RGB_MATRIX_KEYRELEASES // reacts to keyreleases (instead of keypresses)
+// #   define RGB_DISABLE_AFTER_TIMEOUT 0 // number of ticks to wait until disabling effects
+#   define RGB_DISABLE_WHEN_USB_SUSPENDED true // turn off effects when suspended
+#   define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+// #   define RGB_MATRIX_LED_PROCESS_LIMIT (DRIVER_LED_TOTAL + 4) / 5 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
+// #   define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
+#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 150 // limits maximum brightness of LEDs to 150 out of 255. Higher may cause the controller to crash. 
+#    define RGB_MATRIX_HUE_STEP 8
+#    define RGB_MATRIX_SAT_STEP 8
+#    define RGB_MATRIX_VAL_STEP 8
+#    define RGB_MATRIX_SPD_STEP 10
+
+/* Disable the animations you don't want/need.  You will need to disable a good number of these    *
+ * because they take up a lot of space.  Disable until you can successfully compile your firmware. */
+// #   define DISABLE_RGB_MATRIX_ALPHAS_MODS
+// #   define DISABLE_RGB_MATRIX_GRADIENT_UP_DOWN
+// #   define DISABLE_RGB_MATRIX_BREATHING
+// #   define DISABLE_RGB_MATRIX_CYCLE_ALL
+// #   define DISABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
+// #   define DISABLE_RGB_MATRIX_CYCLE_UP_DOWN
+// #   define DISABLE_RGB_MATRIX_CYCLE_OUT_IN
+// #   define DISABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
+// #   define DISABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
+// #   define DISABLE_RGB_MATRIX_DUAL_BEACON
+// #   define DISABLE_RGB_MATRIX_RAINBOW_BEACON
+// #   define DISABLE_RGB_MATRIX_RAINBOW_PINWHEELS
+// #   define DISABLE_RGB_MATRIX_RAINDROPS
+// #   define DISABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
+// #   define DISABLE_RGB_MATRIX_TYPING_HEATMAP
+// #   define DISABLE_RGB_MATRIX_DIGITAL_RAIN
+// #   define DISABLE_RGB_MATRIX_SOLID_REACTIVE
+// #   define DISABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+// #   define DISABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
+// #   define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
+// #   define DISABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
+// #   define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
+// #   define DISABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
+// #   define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
+// #   define DISABLE_RGB_MATRIX_SPLASH
+// #   define DISABLE_RGB_MATRIX_MULTISPLASH
+// #   define DISABLE_RGB_MATRIX_SOLID_SPLASH
+// #   define DISABLE_RGB_MATRIX_SOLID_MULTISPLASH
+#endif
