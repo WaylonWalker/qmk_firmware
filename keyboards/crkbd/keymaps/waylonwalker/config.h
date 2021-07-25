e2976a8a35909a318506b91283b0481c8992d317
Waylon S. Walker <walker_waylon_s@cat.com>
2021-07-25 12:56:57 -0500

    fix tapping terms

--- keyboards/crkbd/keymaps/waylonwalker/config.h
+++ keyboards/crkbd/keymaps/waylonwalker/config.h
@@ -39,9 +39,19 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define USE_SERIAL_PD2
 
-/* #define TAPPING_FORCE_HOLD */
-#define TAPPING_TERM 350
 #define TAPPING_TOGGLE 2
+
+// Configure the global tapping term (default: 200ms)
+#define TAPPING_TERM 200
+
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define TAPPING_FORCE_HOLD
+
+// Apply the modifier on keys that are tapped during a short hold of a modtap
+#define PERMISSIVE_HOLD
 
 #ifdef RGBLIGHT_ENABLE
     #undef RGBLED_NUM
