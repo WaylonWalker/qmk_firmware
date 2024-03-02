c2a780e7018c38a47ba4b6b1dfba0aa4d82f4bdb
Waylon S. Walker <waylon@waylonwalker.com>
2024-03-02 14:25:15 -0600

    format and comment

--- keyboards/crkbd/keymaps/waylonwalker/keymap.c
+++ keyboards/crkbd/keymaps/waylonwalker/keymap.c
@@ -102,16 +102,33 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  // base layer
+  //
+  // inludes modified homerow mods
+  // z super
+  // x alt
+  // d shift
+  // f ctrl
+  // j ctrl
+  // k shift
+  // . alt
+  // / super
+  //
+  //
+  // workspace management
+  // c go to workspace
+  // v switch to workspace
+  // b send window to workspace
   [0] = LAYOUT_split_3x6_3(
-  //,-----------------------------------------------------------------------.                    ,-----------------------------------------------------------------.
-      LT(2, KC_TAB),             KC_Q,    KC_W,    LT(3, KC_E),    KC_R,    LT(12, KC_T),                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
-  //|--------------------------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------------------|
-      LT(7, KC_ESC),             LT(4, KC_A),    LT(5, KC_S),    LSFT_T(KC_D),    LCTL_T(KC_F),    KC_G,                         KC_H,    RCTL_T(KC_J),    RSFT_T(KC_K),    LT(5, KC_L), LT(4, KC_SCLN), LT(7, KC_QUOT),
-  //|--------------------------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------------------|
-      KC_LSFT,                   LGUI_T(KC_Z),    LALT_T(KC_X),    LT(11, KC_C),  LT(9, KC_V),    LT(10, KC_B),                         KC_N,    RCTL_T(KC_M), RSFT_T(KC_COMM),  RALT_T(KC_DOT), RGUI_T(KC_SLSH),  KC_HYPR,
-  //|--------------------------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------------------|
-                                                      KC_LGUI,   KC_LALT,  KC_LCTL,         LT(4, KC_ENT),   LT(2, KC_SPC), KC_RSFT
-                                               //`-----------------------------------'  `---------------------------------------'
+  //,------------------------------------------------------------------------------------------------.     ,--------------------------------------------------------------------------------------------.
+      LT(2, KC_TAB),    KC_Q,             KC_W,           LT(3, KC_E),   KC_R,          LT(12, KC_T),       KC_Y,    KC_U,           KC_I,             KC_O,            KC_P,            KC_BSPC,
+  //|-----------------+-------------+------------------+---------------+--------------+--------------|     |-------+---------------+-----------------+----------------+----------------+----------------|
+      LT(7, KC_ESC),    LT(4, KC_A),      LT(5, KC_S),    LSFT_T(KC_D),  LCTL_T(KC_F),  KC_G,               KC_H,    RCTL_T(KC_J),   RSFT_T(KC_K),     LT(5, KC_L),     LT(4, KC_SCLN),  LT(7, KC_QUOT),
+  //|-----------------+-------------+------------------+---------------+--------------+--------------|     |-------+---------------+-----------------+----------------+----------------+----------------|
+      KC_LSFT,          LGUI_T(KC_Z),     LALT_T(KC_X),   LT(11, KC_C),  LT(9, KC_V),   LT(10, KC_B),       KC_N,    RCTL_T(KC_M),   RSFT_T(KC_COMM),  RALT_T(KC_DOT),  RGUI_T(KC_SLSH), KC_HYPR,
+  //|-----------------+-------------+------------------+---------------+--------------+--------------|     |-------+---------------+--------+--------+----------------+----------------+----------------|
+                                                                       KC_LGUI,   KC_LALT,   KC_LCTL,       LT(4, KC_ENT),   LT(2, KC_SPC),   KC_RSFT
+                                                                   //`----------+----------+----------'   `----------------+----------------+---------'
   ),
 
   // default layer 1 unused
@@ -129,6 +146,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   // used only for symbols, needs merged with layer 3
   [2] = LAYOUT_split_3x6_3(
+  // from base RH Middle Thumb
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
         MO(3), KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -141,7 +159,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   // rgb - mouse
+  // i really don't use this much
   [3] = LAYOUT_split_3x6_3(
+  // from base hold e
+  // from base hole right thumb left and thumb middle (for the rgb stuff)
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
     QK_REBOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_WH_U,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -156,6 +177,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   // paren - numppad
   [4] = LAYOUT_split_3x6_3(
+  // from base layer hold a
+  // from base layer hold semicolon (unused)
+  // from base layer hold right thumb (unused)
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       _______, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, KC_PERC,                     KC_PLUS, KC_7, KC_8, KC_9, KC_PAST, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -168,8 +192,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
 
-  // parens, usused for 4
+  // parens
+  // unused unless I use it by mistake
+  // I use layer 4 instead of this
+  // I do use this layer to tap into minecraft hold s tap q
   [5] = LAYOUT_split_3x6_3(
+  // from base layer hold s
+  // from base layer hold l (unused)
+  //
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       _______, TT(6),   XXXXXXX, KC_LBRC, KC_RBRC, XXXXXXX,                      XXXXXXX, KC_LBRC, KC_RBRC, XXXXXXX, XXXXXXX, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -181,25 +211,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       //`--------------------------'  `--------------------------'
   ),
 
-  // left space for minecraft
+  // left space for minecraft / gaming
   [6] = LAYOUT_split_3x6_3(
-  //,-----------------------------------------------------------------------.                    ,-------------------------------------------------------.
-      LT(1, KC_TAB),             XXXXXXX,    KC_W,    KC_E,    KC_1, KC_T,                        KC_Q,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
-  //|--------------------------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+----------|
-      KC_LSFT,                   KC_A,    KC_S,    KC_D,    KC_F, KC_GRV,                         KC_F3,    KC_MS_BTN2,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
-  //|--------------------------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+----------|
-      MT(MOD_LCTL, KC_ESC),      KC_6,    KC_5,    KC_4,    KC_3, KC_B,                            KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_HYPR,
-  //|--------------------------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+----------|
-                                                  KC_LSFT,   KC_SPC,  KC_LCTL,         LT(1, KC_ENT),   LT(2, KC_SPC), TG(6)
-                                               //`-----------------------------------'  `---------------------------------------'
+  // from base layer hold s press q
+  //,-----------------------------------------------------------------------.                     ,-------------------------------------------------------.
+      LT(1, KC_TAB),             XXXXXXX,    KC_W,    KC_E,    KC_1, KC_T,                         KC_Q,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
+  //|--------------------------+--------+--------+--------+--------+--------|                     |--------+--------+--------+--------+--------+----------|
+      KC_LSFT,                   KC_A,    KC_S,    KC_D,    KC_F, KC_GRV,                          KC_F3,    KC_MS_BTN2,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+  //|--------------------------+--------+--------+--------+--------+--------|                     |--------+--------+--------+--------+--------+----------|
+      MT(MOD_LCTL, KC_ESC),      KC_6,    KC_5,    KC_4,    KC_3, KC_B,                             KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_HYPR,
+  //|--------------------------+--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+----------|
+                                                     KC_LSFT,   KC_SPC,  KC_LCTL,          LT(0, KC_ENT),   LT(2, KC_SPC), TG(6)
+                                               //`-----------------------------------'   `---------------------------------------'
   ),
 
   // vimmy navigation
   [7] = LAYOUT_split_3x6_3(
+  // from base layer hold CAPS (well where a norla CAPSLOCK is)
+  // from base layer hold ' (unused)
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      _______, MO(4), VIM___W, VIM___E, KC_VOLD, KC_VOLU,                    VIM___Y, KC_PGUP,  KC_HOME, VIM___O, VIM___P, KC_DEL,
+      _______, MO(4), VIM___W, VIM___E, KC_VOLD, KC_VOLU,                       VIM___Y, KC_PGUP,  KC_HOME, VIM___O, VIM___P, KC_DEL,
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-     KC_LSFT, KC_END, KC_HOME, LSFT_T(KC_PGDN) , LCTL_T(KC_MUTE),  KC_MPLY,                    LCTL_T( KC_LEFT ), KC_DOWN,   KC_UP,KC_RIGHT,  KC_F11,  KC_F12,
+     KC_LSFT, KC_END, KC_HOME, LSFT_T(KC_PGDN) , LCTL_T(KC_MUTE),  KC_MPLY,     LCTL_T( KC_LEFT ), KC_DOWN,   KC_UP,KC_RIGHT,  KC_F11,  KC_F12,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
      KC_LCTL, XXXXXXX, KC_DEL, XXXXXXX, KC_GRV, VIM___B,                       KC_MNXT, KC_MPRV, KC_LBRC, KC_RBRC, KC_BSLS, KC_TILD,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -207,6 +240,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       //`--------------------------'  `--------------------------'
   ),
   // unused
+  // no way in here
   [8] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       _______, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, KC_MS_WH_UP,                 KC_PLUS, KC_7, KC_8, KC_9, KC_PAST, KC_BSPC,
@@ -218,8 +252,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                           _______, TT(6),  _______,     _______, _______, _______
                                       //`--------------------------'  `--------------------------'
   ),
+
+
+  // Next 3 layers 9, 10, 11 are for navigating workspaces.
+  // This is critical to how I navigate workspaces in pretty much any window
+  // manager
+  //
+  // 9  (x) = go to workspace numpad
+  // 10 (c) = join with workspace numpad
+  // 11 (v) = send window to workspace numpad
+  //
   // awesome wm go to tag numpad
   [9] = LAYOUT_split_3x6_3(
+  // from base layer hold s
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, LCTL(LGUI(KC_7)), LCTL(LGUI(KC_8)), LCTL(LGUI(KC_9)), XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -231,8 +276,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       //`--------------------------'  `--------------------------'
 
   ),
-  // awesome wm join tag numpad
+  // awesome wm join with tag numpad
   [10] = LAYOUT_split_3x6_3(
+  // from base layer hold c
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, LSFT(LGUI(KC_7)), LSFT(LGUI(KC_8)), LSFT(LGUI(KC_9)), XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -245,8 +291,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   ),
 
-  // awesome wm send to tag numpad
+  // awesome wm send window to tag numpad
   [11] = LAYOUT_split_3x6_3(
+  // from base layer hold v
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, LGUI(KC_7), LGUI(KC_8), LGUI(KC_9), XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -261,6 +308,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   // Fkeys
   [12] = LAYOUT_split_3x6_3(
+  // from base layer hold t
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, KC_F7, KC_F8, KC_F9, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
